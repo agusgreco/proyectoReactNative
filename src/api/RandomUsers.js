b6724fa8d35dda8b418a72b14376a88c6cdd7cdf
ema <ejuarez@udesa.edu.ar>
2021-06-04 17:21:00 -0300

    llamado nuevo a api

---
+++ src/api/RandomUsers.js
@@ -0,0 +1,9 @@
+export async function getData() {
+    try {
+        let resultado = await fetch('https://randomuser.me/api?results=20');
+        let json = await resultado.json();
+        return json.results;
+    } catch(e) {
+        console.log('Err: ' + e)
+    }
+}
