19ab1478f4a35eb2e0a0a03d859d63151ea8c0b5
agusgreco <grecoagustina18@gmail.com>
2021-06-25 15:20:07 -0300

    borrando comentarios

--- src/App.js
+++ src/App.js
@@ -35,20 +35,6 @@ export default class App extends Component {
     const {datos} = this.state
 
     return (
-      // <NavigationContainer>
-      //   <Stack.Navigator 
-      //   //   screenOptions={{
-      //   //   headerStyle:{backgroundColor:'black'}
-      //   //   headerTintColor: 'white'
-      //   // }}
-      //   //no me anda!! ayuda
-      //   >
-      //     <Stack.Screen name='Screen Import' component={Screen_Import} options={{title: 'Import'}}/>
-      //     <Stack.Screen name='Screen View Imported Cards' component={Screen_ViewImportedCards} options={{title: 'Imported Cards'}}/>
-      //     {/* <Stack.Screen name="Screen View Imported Cards" component={Screen_ViewImportedCards} initialParams={{valor:0}}/> */}
-      //     <Stack.Screen name='Screen Flatlist' component={Screen_FlatList}/>
-      //   </Stack.Navigator>
-      // </NavigationContainer>
 
     <NavigationContainer>
       <Drawer.Navigator 
@@ -64,13 +50,10 @@ export default class App extends Component {
           activeTintColor: "white",
           inactiveTintColor: 'black',
           labelStyle: {fontSize: 15},
-          // style: { }
           }}
           >
         <Drawer.Screen name='Screen Menu' component={Screen_Menu} options={{title: 'MENU'}}/>
         <Drawer.Screen name='Screen Import' component={Screen_Import} options={{title: 'IMPORTAR'}}/>
-        {/* <Drawer.Screen name='Screen View Imported Cards' component={Screen_ViewImportedCards} options={{title: 'Screen_ViewImportedCards'}}/> */}
-        {/* <Drawer.Screen name="Screen View Imported Cards" component={Screen_ViewImportedCards} initialParams={{valor:0}}/> */}
         <Drawer.Screen name='Screen Flatlist' component={Screen_Flatlist} options={{title: 'IMPORTADAS'}}/>
         <Drawer.Screen name='Screen Recycle' component={Screen_Recycle} options={{title: 'PAPELERA'}}/>
         <Drawer.Screen name='Screen AcercaDe' component={Screen_AcercaDe} options={{title: 'ACERCA DE'}}/>
@@ -80,41 +63,3 @@ export default class App extends Component {
     );
   }
 }
-
-  // componentDidMount(){
-  //         // this.getData();
-  //   fetch("https://randomuser.me/api/?results=9")
-  //   .then(result => result.json())
-  //   .then(data => {
-  //     this.setState({datos: data.results})
-  //     console.log(data)
-  //     console.log(data.results[0].location.postcode)
-  //   })
-  //   // cada contacto tener un boton de guardar, y que cuando lo toques llames a storeData, etc 
-
-  // }
-
-
-
-  // async storeData(key, value){
-  //   //setStringStorage
-  //   try{
-  //     await AsyncStorage.setItem(key, value)
-  //   }catch(error){
-  //     console.log(error);
-  //   }
-  // }
-
-  // async getData(){
-  //   //getStringStorage
-  //   try{
-  //     const item = await AsyncStorage.getItem('');
-  //     if(item !== null){
-  //       this.setState({datos: item});
-  //     }else{
-  //       console.log('no encontre el key')
-  //     }
-  //   }catch(error){
-  //     console.log(error);
-  //   }
-  // }
